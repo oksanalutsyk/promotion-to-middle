47bda5f85ba2d10b744e86cd6aaf62e3802d4c42
Oksana Lutsyk <oksana-lutsyk@Sombra-L54.local>
2021-12-07 12:49:59 +0200

    init structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// components
+import { HeaderComponent } from './modules/core/components/header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
