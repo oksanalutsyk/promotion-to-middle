8e4a4038ef4cd5ac535416113648c4c7b8a352fd
Oksana Lutsyk <oksana-lutsyk@Sombra-L54.local>
2021-12-07 17:20:20 +0200

    added user block, small fix

--- src/app/modules/core/modules/material/material.module.ts
+++ src/app/modules/core/modules/material/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatIconModule],
-  exports: [MatIconModule],
+  imports: [CommonModule, MatIconModule, MatMenuModule],
+  exports: [MatIconModule, MatMenuModule],
 })
 export class MaterialModule {}
