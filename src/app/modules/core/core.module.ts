e5dae8e25768b903218efa9ff60f0a63c0549790
oksana.lutsyk <oksana.lutsyk@sombrainc.com>
2022-02-09 22:47:00 +0200

    fixed bugs

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -9,14 +9,12 @@ import { MaterialModule } from './modules/material/material.module';
 import { HeaderComponent } from './components/header/header.component';
 import { LeftSideMenuComponent } from './components/left-side-menu/left-side-menu.component';
 import { TermsAndPolicyComponent } from './components/terms-and-policy/terms-and-policy.component';
-import { AddArticleComponent } from './forms/add-article/add-article.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     LeftSideMenuComponent,
     TermsAndPolicyComponent,
-    AddArticleComponent
   ],
   imports: [
     CommonModule,
